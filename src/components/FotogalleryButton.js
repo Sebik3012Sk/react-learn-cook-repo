c626a9ba1c107cbcf1f7c97d697b45054327b798
Sebik3012Sk <112552885+Sebik3012Sk@users.noreply.github.com>
2023-06-19 10:17:10 +0000

    codespace

--- src/components/FotogalleryButton.js
+++ src/components/FotogalleryButton.js
@@ -1,6 +1,7 @@
 import "./FotogalleryButton.css";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
 
 const FotogalleryButton = () => {
 
